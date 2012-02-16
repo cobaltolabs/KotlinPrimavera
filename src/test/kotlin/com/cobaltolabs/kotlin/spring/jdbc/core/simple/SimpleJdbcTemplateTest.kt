000a0e917eaa9e50d95d8207b6f486d774868740
Cobalto Labs SAS <mario.arias@cobaltolabs.com>
2012-02-16 13:58:09 -0500

    Import Assert static methods

--- src/test/kotlin/com/cobaltolabs/kotlin/spring/jdbc/core/simple/SimpleJdbcTemplateTest.kt
+++ src/test/kotlin/com/cobaltolabs/kotlin/spring/jdbc/core/simple/SimpleJdbcTemplateTest.kt
@@ -12,7 +12,7 @@ import org.springframework.test.context.ContextConfiguration
 import org.testng.annotations.Test
 import org.springframework.jdbc.core.simple.SimpleJdbcTemplate
 import org.springframework.beans.factory.annotation.Autowired
-import org.testng.Assert
+import org.testng.Assert.*
 import java.util.Map
 import java.util.HashMap
 import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests
@@ -35,11 +35,11 @@ ContextConfiguration class SimpleJdbcTemplateTest:AbstractTransactionalTestNGSpr
     Test fun testQueryForObject() {
         val bean:TestBean = template.sure().queryForObject("select * from test_bean where id = ?", mapper, "python")
 
-        Assert.assertEquals(bean.id, 1)
-        Assert.assertEquals(bean.description, "python")
+        assertEquals(bean.id, 1)
+        assertEquals(bean.description, "python")
     }
 
     Test fun testQueryForInt() {
-        Assert.assertEquals(1, template?.queryForInt("select id from test_bean where description = ?", "python"))
+        assertEquals(1, template?.queryForInt("select id from test_bean where description = ?", "python"))
     }
 }
